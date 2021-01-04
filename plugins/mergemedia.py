54c39fbf8b63d81247cff421c0ada6bc37424e19
Dhruv Maniyaa <71778799+midnightmadwalk@users.noreply.github.com>
2021-01-04 17:33:09 +0530

    formatting ...

--- plugins/mergemedia.py
+++ plugins/mergemedia.py
@@ -7,13 +7,15 @@
 
 import os
 import re
+import codecs
 
 import shutil
 from pathlib import Path
 
 from userge import Message, userge
 from userge.plugins.misc.upload import upload
-from userge.utils import runcmd, progress
+from userge.utils import progress, runcmd
+
 
 @userge.on_cmd(
     "mergesave",
@@ -23,16 +25,23 @@ from userge.utils import runcmd, progress
     },
 )
 async def mergesave_(message: Message):
-  """mergesave"""
-  # saving files in a separate folder.
-  await message.edit("`downloading ...`", del_in=15)
-  try:
-      o_o = await message.client.download_media(message=message.reply_to_message, file_name='merge/', progress=progress, progress_args=(message, "`Saving for further merge !`"),
-      )
-  except AttributeError:
-      await message.err("Reply To Media,dear.")
-  else:
-      await message.edit(f"Saved in {o_o}")
+    """mergesave"""
+    # saving files in a separate folder.
+    await message.edit("`downloading ...`")
+    try:
+        o_o = await message.client.download_media(
+          message=message.reply_to_message,
+          file_name='merge/',
+          progress=progress,
+          progress_args=(
+            message,
+            "`Saving for further merge !`"
+          ),
+        )
+    except AttributeError:
+        await message.err("Reply To Media,dear.")
+    else:
+        await message.edit(f"Saved in {o_o}")
 
 
 @userge.on_cmd(
@@ -46,20 +55,22 @@ async def merge_(message: Message):
     """MergeMedia with FFmpeg"""
     # preparing text file.
     await message.edit("`🙂🙃 Preparing text file ...`")
-    x_x = open("merge.txt", "w+")
+    x_x = codecs.open("merge.txt", "w+", "utf-8")
     for media in os.listdir("merge"):
-        data_ = "file" +" "+ "'" + "merge/" + media + "'" +"\n"
+        data_ = "file" + " " + "'" + "merge/" + media + "'" + "\n"
         x_x.write(data_)
     x_x.close()
     # detecting extension.
     await message.edit("`😎🥲 detecting extension ...`")
     for ext in os.listdir("merge")[:1]:
         a_a, b_b = re.findall("[^.]*$", ext)
-        output_path = "merge/output."+a_a
+        output_path = "merge/output." + a_a
     await message.edit(f"detected extension is .{a_a}")
     # ffmpeg.
-    await message.edit("`🏃️🏃 ffmpeg ...`")
-    await runcmd(f"ffmpeg -f concat -i merge.txt -map 0 -c copy -scodec copy {output_path}")
+    await message.edit("`🏃️🏃🏃 ffmpeg ...`")
+    await runcmd(
+      f'''ffmpeg -f concat -safe 0 -i merge.txt -map 0 -c copy -scodec copy {output_path}'''
+    )
     # upload.
     await upload(message, Path(output_path))
     # cleanup.
